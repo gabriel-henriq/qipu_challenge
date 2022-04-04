bc4466d215819dd71f14ef57d1875774cc52682d
Gabriel Henrique <gabrielhcm01@gmail.com>
2022-04-04 00:33:57 -0300

    Adding test option, fixing function name on metar and taf functions and fixing spliting array in get_cards function to prevent getting wrong fields.

--- Task_3/__init__.py
+++ Task_3/__init__.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,25 +11,127 @@ Vale ressaltar que estas informações devem ser obtidas em tempo real do site, 
 """
 
 
-def validate(doc: BeautifulSoup):
+def test_icaos() -> None:
+
+    icao_tests = [
+        "SWYN",
+        "SNAL",
+        "SWBC",
+        "SWBI",
+        "SBBE",
+        "SBCF",
+        "SBBH",
+        "SWBR",
+        "SWBS",
+        "SBBR",
+        "SBCD",
+        "SNCC",
+        "SBKP",
+        "SDAM",
+        "SBMT",
+        "SNRU",
+        "SWCA",
+        "SBCA",
+        "SILQ",
+        "SWKO",
+        "SBAA",
+        "SBCZ",
+        "SBBI",
+        "SBCT",
+        "SWFJ",
+        "SWFN",
+        "SBFL",
+        "SBFZ",
+        "SBFI",
+        "SBZM",
+        "SBGO",
+        "SBGR",
+        "SBIZ",
+        "SBJE",
+        "SBJV",
+        "SBJP",
+        "SBJF",
+        "SBJD",
+        "SBMQ",
+        "SBEG",
+        "SNML",
+        "SBMO",
+        "SBMS",
+        "SBNF",
+        "SBSG",
+        "SWNK",
+        "SBOI",
+        "SNOZ",
+        "SWJV",
+        "SBPB",
+        "SNPE",
+        "SSZW",
+        "SBPA",
+        "SNPG",
+        "SBRF",
+        "SBRP",
+        "SBRB",
+        "SBRJ",
+        "SBGL",
+        "SBJR",
+        "SBRD",
+        "SBSM",
+        "SDOE",
+        "SBST",
+        "SDSC",
+        "SBSL",
+        "SBSP",
+        "SWSN",
+        "SDCO",
+        "SWMU",
+        "SBTT",
+        "SBTK",
+        "SBTF",
+        "SBTE",
+        "SBTS",
+        "SWXU",
+    ]
+
+    for test in icao_tests:
+        url = f"https://aisweb.decea.mil.br/?i=aerodromos&codigo={test}"
+        page = requests.get(url).text
+        doc = BeautifulSoup(page, "html.parser")
+        data = dict()
+
+        try:
+            validate(doc)
+            data["sunrise_sunset"] = get_sunrise_sunset(doc)
+            data["metar"] = get_metar(doc)
+            data["taf"] = get_taf(doc)
+            data["cards"] = get_cards(doc)
+            with open(f"{test}.json", "w") as file:
+                json.dump(data, file, indent=2)
+                print(f"{test} salvo com sucesso.")
+        except Exception as e:
+            print(f"{e}: {test}")
+
+
+def validate(doc: BeautifulSoup) -> None:
     bad_request = doc.find(text="We're sorry - An Error Occurred")
     not_found = doc.find(class_="alert alert-danger")
     if bad_request:
-        raise Exception("O aeródromo não foi encontrado.")
+        raise Exception("\nO aeródromo não foi encontrado.")
     elif not_found:
         if not_found.text.strip() == "O aeródromo não foi encontrado.":
-            raise Exception("O aeródromo não foi encontrado.")
+            raise Exception("\nO aeródromo não foi encontrado.")
 
 
 def get_cards(doc: BeautifulSoup) -> dict:
 
     cards = doc.find("hr", id="cartas").find_next_siblings("h4")
     cards_len = len(cards)
-    if cards_len <= 3:
-        cards = doc.find("hr", id="cartas").find_next_siblings("h4")[1:]
-    else:
-        cards = doc.find("hr", id="cartas").find_next_siblings("h4")[1:-1]
+    if cards_len <= 3 and cards_len != 2:
+        cards = doc.find("hr", id="cartas").find_next_siblings("h4")[1:2]
 
+    elif cards_len > 3:
+        cards = doc.find("hr", id="cartas").find_next_siblings("h4")[1:-1]
+    else:
+        return
     cards_list = []
     cards_text = []
     urls = []
@@ -71,38 +174,41 @@ def get_metar(doc: BeautifulSoup) -> str:
 
     metar = doc.find("h5", text="METAR")
     if metar:
-
-        metar = doc.find("h5", text="METAR")
-        return metar
+        return metar.find_next_sibling("p").text.strip()
     else:
         return "Não encontrado."
 
 
 def get_taf(doc: BeautifulSoup) -> str:
 
-    taf = doc.find("h5", text="taf")
+    taf = doc.find("h5", text="TAF")
     if taf:
-        return taf.next_sibling("p").text.strip()
-
+        return taf.find_next_sibling("p").text.strip()
     else:
         return "Não encontrado."
 
 
 def print_data(data: dict) -> None:
-    print(f"\nSunrise: {data['sunrise_sunset']['sunrise']}\n")
-    print(f"Sunset: {data['sunrise_sunset']['sunset']}\n")
-    print(f"Metar: {data['metar']}\n")
-    print(f"Taf: {data['taf']}\n")
-    print("Cards: ")
-    for card in data["cards"]:
-        print(f"\t{card}:")
-        for link in data["cards"][card]:
-            print(f"\t\t{link}: {data['cards'][card][link]}")
+    print(f"\nNascer do Sol: {data['sunrise_sunset']['sunrise']}\n")
+    print(f"Por do Sol: {data['sunrise_sunset']['sunset']}\n")
+    print(f"METAR: {data['metar']}\n")
+    print(f"TAF: {data['taf']}\n")
+    print("Cartas: ")
+    if isinstance(data["cards"], dict):
+        for card in data["cards"]:
+            print(f"\t{card}:")
+            for link in data["cards"][card]:
+                print(f"\t\t{link}: {data['cards'][card][link]}")
 
 
 if __name__ == "__main__":
-    while True:
-        icao = str(input("\nDigite um ICAO:"))
+
+    test_mode = False
+
+    if test_mode:
+        test_icaos()
+    else:
+        icao = str(input("\nDigite um ICAO: "))
         url = f"https://aisweb.decea.mil.br/?i=aerodromos&codigo={icao}"
         page = requests.get(url).text
         doc = BeautifulSoup(page, "html.parser")
